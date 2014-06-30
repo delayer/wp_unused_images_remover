f9e486b0786d35158a11245706d49ca7f1fef067
Delayer <volik.a.a@gmail.com>
2014-06-30 23:34:56 +0300

    V. 1.0

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 /** Имя пользователя MySQL */
-define('DB_USER', 'pr_foodrecept');
+define('DB_USER', '');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', 'TuigErT8');
+define('DB_PASSWORD', '');
 
 /** Имя сервера MySQL */
 define('DB_HOST', 'localhost');
